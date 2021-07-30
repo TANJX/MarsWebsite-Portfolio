1d8aabf1b4f235dc2f4dfb93de2ce8be81022682
marstan(谭健旋) <marstan@tencent.com>
2021-07-30 10:59:01 +0800

    Dev notification

--- script/dev.js
+++ script/dev.js
@@ -1,8 +1,10 @@
-
+const notifier = require('node-notifier');
 const sass = require('sass');
 const pug = require('pug');
 const fs = require('fs');
 const fw = require('filewatcher');
+
+notifier.notify('Start Mars Inc. debugging.');
 
 function getAllFiles(path, ext) {
   const files = [];
@@ -34,27 +36,68 @@ watcher.add('./template');
 
 console.log('Start listening\n');
 
+let styleError = false;
+let pugError = false;
+
 watcher.on('change', function (file, stat) {
+  const errorFiles = [];
   if (file === './template') {
     console.log('Compile templates');
+
     for (const pugFile of pugFiles) {
       console.log(`Rendering ${pugFile}.pug`);
-      const html = pug.renderFile(`./template/${pugFile}.pug`, {});
-      console.log(`Writing ${pugFile}.html`);
-      fs.writeFileSync(`./dist/${pugFile}.html`, html);
+      try {
+        const html = pug.renderFile(`./template/${pugFile}.pug`, {});
+        console.log(`Writing ${pugFile}.html`);
+        fs.writeFileSync(`./dist/${pugFile}.html`, html);
+      } catch (e) {
+        pugError = true;
+        errorFiles.push(`${pugFile}.pug`);
+        console.log(`Error: ${pugFile}.pug`);
+        console.log(e);
+      }
     }
   } else if (file === './style') {
     console.log('Compile style');
+
     for (const styleFile of styleFiles) {
       console.log(`Rendering ${styleFile}.scss`);
-      const result = sass.renderSync({
-        file: `./style/${styleFile}.scss`,
-        sourceMap: true,
-        outFile: `${styleFile}.css`,
+      try {
+        const result = sass.renderSync({
+          file: `./style/${styleFile}.scss`,
+          sourceMap: true,
+          outFile: `${styleFile}.css`,
+        });
+        console.log(`Writing ${styleFile}.css`);
+        fs.writeFileSync(`./dist/style/${styleFile}.css`, result.css);
+        fs.writeFileSync(`./dist/style/${styleFile}.css.map`, result.map);
+      } catch (e) {
+        styleError = true;
+        errorFiles.push(`${styleFile}.scss`);
+        console.log(`Error: ${styleFile}.scss`);
+        console.log(e);
+      }
+    }
+  }
+  if (errorFiles.length > 0) {
+    notifier.notify({
+      title: 'Compile Error',
+      message: errorFiles.join(', '),
+    });
+  } else {
+    if (styleError) {
+      styleError = false;
+      notifier.notify({
+        title: 'SASS Fixed',
+        message: 'Styles were compiled successfully',
       });
-      console.log(`Writing ${styleFile}.css`);
-      fs.writeFileSync(`./dist/style/${styleFile}.css`, result.css);
-      fs.writeFileSync(`./dist/style/${styleFile}.css.map`, result.map);
+    }
+    if (pugError) {
+      pugError = false;
+      notifier.notify({
+        title: 'Pug Fixed',
+        message: 'Templates were compiled successfully',
+      });
     }
   }
   console.log('Updated\n');
